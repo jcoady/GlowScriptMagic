32f9024692039851b24a7c6c56b32846c8e8c4f0
john <johncoady@shaw.ca>
2016-06-16 12:31:33 -0700

    add acorn.js require
    add acorn.js require

--- glowscriptmagic.py
+++ glowscriptmagic.py
@@ -11,7 +11,7 @@ def glowscript(line, cell):
             require(['https://cdnjs.cloudflare.com/ajax/libs/acorn/3.2.0/acorn.js','https://dl.dropboxusercontent.com/u/5095342/glowscript/lib/jquery/2.1/jquery-ui.custom.min.js','https://dl.dropboxusercontent.com/u/5095342/glowscript/package/compiler.2.1.min.js','https://dl.dropboxusercontent.com/u/5095342/glowscript/package/symbols.2.1.min.js','https://dl.dropboxusercontent.com/u/5095342/glowscript/package/RSrun.2.1.min.js','https://dl.dropboxusercontent.com/u/5095342/glowscript/package/RScompiler.2.1.min.js','https://dl.dropboxusercontent.com/u/5095342/glowscript/package/glow.2.1.min.js'], function() {
             var cell_content = """+json.dumps(cell)+""";
             var embedScript = window.glowscript_compile(cell_content, {lang:'"""+lang+"""'}); 
-            embedScript = "require(['https://dl.dropboxusercontent.com/u/5095342/glowscript/lib/jquery/2.1/jquery-ui.custom.min.js','https://dl.dropboxusercontent.com/u/5095342/glowscript/package/glow.2.1.min.js'], function() {" + embedScript + ";$(function(){ window.__context = { glowscript_container: $('#glowscript').removeAttr('id') }; main() });})";
+            embedScript = "require(['https://cdnjs.cloudflare.com/ajax/libs/acorn/3.2.0/acorn.js','https://dl.dropboxusercontent.com/u/5095342/glowscript/lib/jquery/2.1/jquery-ui.custom.min.js','https://dl.dropboxusercontent.com/u/5095342/glowscript/package/glow.2.1.min.js'], function() {" + embedScript + ";$(function(){ window.__context = { glowscript_container: $('#glowscript').removeAttr('id') }; main() });})";
             embedScript = embedScript.replace("</", "<\\/"); // escape anything that could be a close script tag... hopefully this sequence only occurs in strings!
             eval(embedScript);
             })
@@ -19,7 +19,7 @@ def glowscript(line, cell):
             require(['http://cdnjs.cloudflare.com/ajax/libs/acorn/3.2.0/acorn.js','http://www.glowscript.org/lib/jquery/2.1/jquery.min.js','http://www.glowscript.org/lib/jquery/2.1/jquery-ui.custom.min.js','http://www.glowscript.org/package/compiler.2.1.min.js','http://www.glowscript.org/package/symbols.2.1.min.js','http://www.glowscript.org/package/RSrun.2.1.min.js','http://www.glowscript.org/package/RScompiler.2.1.min.js','http://www.glowscript.org/package/glow.2.1.min.js'], function() {
             var cell_content = """+json.dumps(cell)+""";
             var embedScript = window.glowscript_compile(cell_content, {lang:'"""+lang+"""'}); 
-            embedScript = "require(['http://www.glowscript.org/lib/jquery/2.1/jquery-ui.custom.min.js','http://www.glowscript.org/package/glow.2.1.min.js'], function() {" + embedScript + ";$(function(){ window.__context = { glowscript_container: $('#glowscript').removeAttr('id') }; main() });})";
+            embedScript = "require(['http://cdnjs.cloudflare.com/ajax/libs/acorn/3.2.0/acorn.js','http://www.glowscript.org/lib/jquery/2.1/jquery-ui.custom.min.js','http://www.glowscript.org/package/glow.2.1.min.js'], function() {" + embedScript + ";$(function(){ window.__context = { glowscript_container: $('#glowscript').removeAttr('id') }; main() });})";
             embedScript = embedScript.replace("</", "<\\/"); // escape anything that could be a close script tag... hopefully this sequence only occurs in strings!
             eval(embedScript);
             })
@@ -27,7 +27,7 @@ def glowscript(line, cell):
             require(['http://cdnjs.cloudflare.com/ajax/libs/acorn/3.2.0/acorn.js','http://www.glowscript.org/lib/jquery/2.1/jquery-ui.custom.min.js','http://www.glowscript.org/package/compiler.2.1.min.js','http://www.glowscript.org/package/symbols.2.1.min.js','http://www.glowscript.org/package/RSrun.2.1.min.js','http://www.glowscript.org/package/RScompiler.2.1.min.js','http://www.glowscript.org/package/glow.2.1.min.js'], function() {
             var cell_content = """+json.dumps(cell)+""";
             var embedScript = window.glowscript_compile(cell_content, {lang:'"""+lang+"""'}); 
-            embedScript = "require(['http://www.glowscript.org/lib/jquery/2.1/jquery-ui.custom.min.js','http://www.glowscript.org/package/glow.2.1.min.js'], function() {" + embedScript + ";$(function(){ window.__context = { glowscript_container: $('#glowscript').removeAttr('id') }; main() });})";
+            embedScript = "require(['http://cdnjs.cloudflare.com/ajax/libs/acorn/3.2.0/acorn.js','http://www.glowscript.org/lib/jquery/2.1/jquery-ui.custom.min.js','http://www.glowscript.org/package/glow.2.1.min.js'], function() {" + embedScript + ";$(function(){ window.__context = { glowscript_container: $('#glowscript').removeAttr('id') }; main() });})";
             embedScript = embedScript.replace("</", "<\\/"); // escape anything that could be a close script tag... hopefully this sequence only occurs in strings!
             eval(embedScript);
             })
